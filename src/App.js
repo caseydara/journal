b59e39d2ed15de927deafca64a45d06e3d5ae6d5
Dara Casey <dara.e.casey.25@dartmouth.edu>
2025-09-04 17:41:54 -0400

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // src/App.js
-import React from 'react';
-import Assistant from './components/Assistant';
-import History from './components/History';
-import HumeDemoPage from './components/HumeDemo';
-import { VoiceProvider } from '@humeai/voice-react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Assistant from "./components/Assistant";
+import History from "./components/History";
+import HumeDemoPage from "./components/HumeDemo";
+import { VoiceProvider } from "@humeai/voice-react";
 
 export default function App() {
-  const path = window.location.pathname;
-  if (path === '/history') return <History />;
-  if (path === '/hume-demo') return <HumeDemoPage />;
-  return(<VoiceProvider><Assistant /></VoiceProvider>);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/history" element={<History />} />
+        <Route path="/hume-demo" element={<HumeDemoPage />} />
+        <Route path="/" element={<VoiceProvider><Assistant /></VoiceProvider>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
